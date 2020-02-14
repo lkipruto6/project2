616b97cd43874750676f70eb60996c94d9a280f7
Santa Claus <santa@northpole.com>
2020-02-14 09:58:56 +0300

    change in javascript style

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function identifyAkanName () {
+    var days = ("sunday" "monday" "tuesday" "wednesday" "thursday" "friday" "saturday")
+    var maleAkanNames = ("kwasi" "kwadwo" "kabena" "kwaku" "yaw" "kofi" "kwame")
+    var femaleAkanNames = ("akosu" "adwoa" "abeena" "akua" "yaa" "afua" "amna")
+}
